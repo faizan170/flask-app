c7f7ff5d6d14321fbfa74895c1ed8558b1c299ee
Faizan Amin <faizanfaizanamin@gmail.com>
2020-10-06 17:42:56 +0500

    file upload feature added

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from flask import Flask
-
+from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "App is working"
+    return render_template("index.html")
 
+
+
+@app.route("/upload", methods=["POST"])
+def upload_request():
+    try:
+        fileData = request.files["file"]
+        filename = secure_filename(fileData.filename)
+        fileData.save("images/" + filename)
+        return "File Upload Successfully"
+    except Exception as ex:
+        return str(ex)
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=True)
